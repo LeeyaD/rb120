da6881fe5a9be83b3580add43e220fb0038c9efc
Leeya Davis <leeya463@gmail.com>
2021-10-04 21:56:44 -0400

    finished lecture_classes_and_objects

--- lesson02_oop/lecture_classes_and_objects.rb
+++ lesson02_oop/lecture_classes_and_objects.rb
@@ -29,12 +29,12 @@
 #   end
 # end
 
-bob = Person.new('Robert')
-puts bob.name                  # => 'Robert'
-puts bob.first_name            # => 'Robert'
-puts bob.last_name             # => ''
-bob.last_name = 'Smith'
-puts bob.name                  # => 'Robert Smith'
+# bob = Person.new('Robert')
+# puts bob.name                  # => 'Robert'
+# puts bob.first_name            # => 'Robert'
+# puts bob.last_name             # => ''
+# bob.last_name = 'Smith'
+# puts bob.name                  # => 'Robert Smith'
 # -----------------------------------------------------------
 
 # Problem 3
@@ -43,23 +43,53 @@ puts bob.name                  # => 'Robert Smith'
 class Person
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name = "")
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(full_name)
+    parse_full_name(full_name)
+  end
+
+  def name=(n)
+    parse_full_name(n)
   end
 
   def name
     @first_name + ' ' + @last_name
   end
+
+  def to_s # Problem 5: Let's add a to_s method to the class
+    name
+  end
+  private
+
+  def parse_full_name(full_name)
+    if full_name.split.size == 1
+      self.first_name = full_name
+      self.last_name = ''
+    else
+      @first_name, @last_name = full_name.split
+    end
+  end
 end
 
 bob = Person.new('Robert')
 bob.name                  # => 'Robert'
-bob.first_name            # => 'Robert'
-bob.last_name             # => ''
+puts bob.first_name            # => 'Robert'
+puts bob.last_name             # => ''
 bob.last_name = 'Smith'
-bob.name                  # => 'Robert Smith'
+puts bob.name                  # => 'Robert Smith'
 
 bob.name = "John Adams"
-bob.first_name            # => 'John'
-bob.last_name             # => 'Adams'
+puts bob.first_name            # => 'John'
+puts bob.last_name             # => 'Adams'
+# -----------------------------------------------------------
+
+# Problem 4
+# Using the class definition from step #3, let's create a few more people -- that is, Person objects.
+bob = Person.new('Robert Smith')
+rob = Person.new('Robert Smith')
+# If we're trying to determine whether the two objects contain the same name, how can we compare the two objects?
+
+puts bob.name == rob.name
+puts bob
+# -----------------------------------------------------------
+
+# Problem 5 (see above, #3)
