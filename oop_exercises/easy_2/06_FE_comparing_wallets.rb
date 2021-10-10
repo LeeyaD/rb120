18ea37ae09eb68342d38990baadc3a742e0e5902
Leeya Davis <leeya463@gmail.com>
2021-10-10 12:20:38 -0400

    finished easy2 #6 & #6 FE

---
+++ oop_exercises/easy_2/06_FE_comparing_wallets.rb
@@ -0,0 +1,4 @@
+# This example is rather contrived and unrealistic, but this type of situation occurs frequently in applications. Can you think of any applications where protected methods would be desirable?
+
+# when comparing address distances from where you currently are
+# a medical application that stores client data
