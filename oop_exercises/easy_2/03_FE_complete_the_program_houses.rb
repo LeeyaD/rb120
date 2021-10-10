b96600258ef5895347ee1ffa0b28645afaf1cd64
Leeya Davis <leeya463@gmail.com>
2021-10-10 12:03:41 -0400

    finished easy2 #1-3 & #3 FE

---
+++ oop_exercises/easy_2/03_FE_complete_the_program_houses.rb
@@ -0,0 +1,7 @@
+# Is the technique we employ here to make House objects comparable a good one? (Hint: is there a natural way to compare Houses? Is price the only criteria you might use?) 
+# since the output was re money 'cheaper'/'expensice' the technique we used is a good one.
+# a more natural way??? None that I can think of...
+
+# What problems might you run into, if any? Can you think of any sort of classes where including Comparable is a good idea?
+# We'll run into problems if we want to compare Houses based on other attributes...
+# Classes that have one data type 'string', 'integer' but not the possibility of both
