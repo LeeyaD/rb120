32f83aed316b93b2cae23538aa1c4be5f3a94ada
Leeya Davis <leeya463@gmail.com>
2021-10-29 22:50:37 -0400

    completed lesson4 #5 practice problem

---
+++ lesson04_practice_problems/easy_02/q05.rb
@@ -0,0 +1,6 @@
+# There are a number of variables listed below. What are the different types and how do you know which is which?
+# A: based on their syntax
+# BETTER A: based on their prefixes
+excited_dog = "excited dog" # A: local variable
+@excited_dog = "excited dog" # A: instance variable
+@@excited_dog = "excited dog" # A: class variable
