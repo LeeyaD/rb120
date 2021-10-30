8ff97d8668f46a86f99a8b745aeddc1c4400745c
Leeya Davis <leeya463@gmail.com>
2021-10-29 23:15:15 -0400

    completed lesson4 easy2 #10 pp

---
+++ lesson04_practice_problems/easy_02/q10.rb
@@ -0,0 +1,4 @@
+# What are the benefits of using Object Oriented Programming in Ruby? Think of as many as you can.
+# Our entire program is sectioned off into parts and so when a change needs to be made, we can make that change without risking throwing our entire program out of whack
+# Makes our code more organized
+# DRYs up out code
