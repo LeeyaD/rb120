2c16f955b606747a0e14faad5b42d32e787a347e
Leeya Davis <leeya463@gmail.com>
2021-10-31 16:31:21 -0400

    finished lesson4 hard1 #3 & 4 pp

---
+++ lesson04_practice_problems/hard_01/q03.rb
@@ -0,0 +1,61 @@
+# Building on the prior vehicles question, we now must also track a basic motorboat. A motorboat has a single propeller and hull, but otherwise behaves similar to a catamaran. Therefore, creators of Motorboat instances don't need to specify number of hulls or propellers. How would you modify the vehicles code to incorporate a new Motorboat class?
+
+# Peaked at the solution and saw 'class Seacraft'
+# With that hint, create a superclass w/ Catamaran & Motoboat as subclasses
+module Moveable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_capacity, :fuel_efficiency
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Moveable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+end
+
+class Seacraft
+  include Moveable
+  attr_reader :propeller_count, :hull_count
+
+  def initialize( km_traveled_per_liter, liters_of_fuel_capacity, num_propellers = 1, num_hulls = 1)
+    @propeller_count = num_propellers
+    @num_hulls = num_hulls
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+end
+
+class Catamaran
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
+
+class Motorboat
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    super
+  end
+end
+
+class Motorboat < Seacraft
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    # set up 1 hull and 1 propeller
+    super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
