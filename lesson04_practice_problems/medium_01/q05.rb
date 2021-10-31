6ebf4543b2a711e42402c5a723a14527680ca7df
Leeya Davis <leeya463@gmail.com>
2021-10-31 09:09:23 -0400

    finished lesson4 medium1 #5 pp

---
+++ lesson04_practice_problems/medium_01/q05.rb
@@ -0,0 +1,46 @@
+# You are given the following class that has been implemented:
+class KrispyKreme
+  attr_reader :filling_type, :glazing
+
+  def initialize(filling_type, glazing)
+    @filling_type = filling_type
+    @glazing = glazing
+    format_filling
+  end
+
+  def format_filling
+    @filling_type = 'Plain' if filling_type == nil
+  end
+
+  def to_s
+    if glazing
+      "#{filling_type}" + " with #{glazing}"
+    else
+      "#{filling_type}"
+    end
+  end
+
+  # SOLUTION: better readability
+  # def to_s
+  #   filling_string = @filling_type ? @filling_type : "Plain"
+  #   glazing_string = @glazing ? " with #{@glazing}" : ''
+  #   filling_string + glazing_string
+  # end
+end
+# And the following specification of expected behavior:
+donut1 = KrispyKreme.new(nil, nil)
+donut2 = KrispyKreme.new("Vanilla", nil)
+donut3 = KrispyKreme.new(nil, "sugar")
+donut4 = KrispyKreme.new(nil, "chocolate sprinkles")
+donut5 = KrispyKreme.new("Custard", "icing")
+
+puts donut1 #=> "Plain"
+
+puts donut2 #=> "Vanilla"
+
+puts donut3 #=> "Plain with sugar"
+
+puts donut4 #=> "Plain with chocolate sprinkles"
+
+puts donut5 #=> "Custard with icing"
+# Write additional code for KrispyKreme such that the puts statements will work as specified above.
