f72202259e5ddf109ac30517a8d25dc75a7172db
Leeya Davis <leeya463@gmail.com>
2021-10-30 20:42:28 -0400

    completed lesson4 pp easy3 #7

--- lesson04_practice_problems/easy_03/q07.rb
+++ lesson04_practice_problems/easy_03/q07.rb
@@ -10,5 +10,6 @@ class Light
   def self.information
     return "I want to turn on the light with a brightness level of super high and a color of green"
   end
-
 end
+# A: the instance variables 'brightness' and 'color' we don't do anything with them
+# CORRECT A: the keyword 'return' since Ruby evaluates the last line of code in a method
