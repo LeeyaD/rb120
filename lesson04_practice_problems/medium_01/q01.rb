8278a14b6e4de6b759fef6cd38c5ece19a3901ac
Leeya Davis <leeya463@gmail.com>
2021-10-30 22:12:24 -0400

    completed lesson4 medium1 #1 pp

---
+++ lesson04_practice_problems/medium_01/q01.rb
@@ -0,0 +1,18 @@
+# Ben asked Alyssa to code review the following code:
+class BankAccount
+  attr_reader :balance
+
+  def initialize(starting_balance)
+    @balance = starting_balance
+  end
+
+  def positive_balance?
+    balance >= 0
+  end
+end
+# Alyssa glanced over the code quickly and said - "It looks fine, except that you forgot to put the @ before balance when you refer to the balance instance variable in the body of the positive_balance? method."
+
+# "Not so fast", Ben replied. "What I'm doing here is valid - I'm not missing an @!"
+
+# Who is right, Ben or Alyssa, and why?
+# A: Ben is right because we have an attr_reader which gives us our getter method, which returns the data store in our instance variable. No need to reference it directly!
