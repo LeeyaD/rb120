255964a0888e9d589039b0f26b090c4c00bf5a0c
Leeya Davis <leeya463@gmail.com>
2021-11-02 09:52:35 -0400

    improved the #winning_marker method

--- lesson05_larger_oo_programs/oo_ttt.rb
+++ lesson05_larger_oo_programs/oo_ttt.rb
@@ -33,21 +33,12 @@ class Board
   def someone_won?
     !!winning_marker
   end
-  
-  def count_human_marker(squares)
-    squares.collect(&:marker).count(TTTGame::HUMAN_MARKER)
-  end
-
-  def count_computer_marker(squares)
-    squares.collect(&:marker).count(TTTGame::COMPUTER_MARKER)
-  end
 
   def winning_marker
     WINNING_LINES.each do |line|
-      if count_human_marker(@squares.values_at(*line)) == 3
-        return TTTGame::HUMAN_MARKER
-      elsif count_computer_marker(@squares.values_at(*line)) == 3
-        return TTTGame::COMPUTER_MARKER
+      squares = @squares.values_at(*line)
+      if three_identical_markers?(squares)
+        return squares.first.marker
       end
     end
     nil
@@ -70,6 +61,16 @@ class Board
     puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}"
     puts "     |     |"
   end
+
+  private
+
+  def three_identical_markers?(squares)
+    if squares.all? { |sq| sq.marked? }
+      squares.collect(&:marker).all? do |sq| 
+        sq == squares.first.marker
+      end
+    end
+  end
 end
 
 class Square
@@ -87,6 +88,10 @@ class Square
 
   def unmarked?
     marker == INITIAL_MARKER
+  end
+
+  def marked?
+    marker != INITIAL_MARKER
   end
 end
 
