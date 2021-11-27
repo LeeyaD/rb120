da1e6e05fedaf073a2a9baba36ed23f60884dd33
Leeya Davis <leeya463@gmail.com>
2021-11-27 11:31:17 -0500

    finished oop exercise debugging #10

--- oop_exercises/debugging/10_does_it_rock_or_not.rb
+++ oop_exercises/debugging/10_does_it_rock_or_not.rb
@@ -1,5 +1,6 @@
 # We discovered Gary Bernhardt's repository for finding out whether something rocks or not, and decided to adapt it for a simple example.
-class AuthenticationError < Exception; end
+class AuthenticationError < StandardError; end
+# When creating custom exceptions & when rescuing exceptions, good practice is to always use the subclass StandardError. StandardError subsumes all application-level errors. The other descendants of Exception are used for system- or environment-level errors, like memory overflows or program interruptions. These are the kind of errors your application usually does not want to throw - and definitely does not want to rescue, they should be handled by Ruby itself.
 
 # A mock search engine
 # that returns a random number instead of an actual count.
@@ -31,8 +32,8 @@ module DoesItRock
       negative = SearchEngine.count(%{"#{term} is not fun"}, API_KEY).to_f
 
       positive / (positive + negative)
-    rescue Exception
-      NoScore
+    rescue ZeroDivisionError # the only error we really want to rescue here
+      NoScore.new # needs to be an instance of the class!
     end
   end
 
@@ -49,7 +50,7 @@ module DoesItRock
     else
       "#{term} rocks!"
     end
-  rescue Exception => e
+  rescue StandardError => e
     e.message
   end
 end
@@ -61,7 +62,3 @@ puts DoesItRock.find_out('Rain')        # Rain is not fun.
 puts DoesItRock.find_out('Bug hunting') # Bug hunting rocks!
 
 # In order to test the case when authentication fails, we can simply set API_KEY to any string other than the correct key. Now, when using a wrong API key, we want our mock search engine to raise an AuthenticationError, and we want the find_out method to catch this error and print its error message API key is not valid.
-
-# Is this what you expect to happen given the code?
-
-# And why do we always get the following output instead?
